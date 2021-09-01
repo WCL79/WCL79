dfa868e4f47f754ad107c68f9bb881703bb4a4dd
Weslley Lima <72285106+WCL79@users.noreply.github.com>
2021-09-01 19:24:18 -0300

    Update contador.js

--- contador.js
+++ contador.js
@@ -1,19 +1,60 @@
-jQuery(document).ready(function() {
-	function count($this){
-		var current = parseInt($this.html(), 10);
-		current = current + 1; /* Where 1 is increment */
 
-		$this.html(++current);
-		if(current > $this.data('count')){
-			$this.html($this.data('count'));
-		} else {
-			setTimeout(function(){count($this)}, 50);
-		}
-	}
+function ctck()
+{
+var sds = document.getElementById("dum");
+if(sds == null){
+alert("Esta utilizando un paquete gratuito. No esta autorizado para retirar la etiqueta.");
+}
+var sdss = document.getElementById("dumdiv");
+if(sdss == null){
+alert("Esta utilizando un paquete gratuito. No esta autorizado para retirar la etiqueta.");
+}
+}
+document.onload="ctck()";
+function getCookieVal (offset)
+{
+var endstr = document.cookie.indexOf (";", offset);
+if (endstr == -1)
+endstr = document.cookie.length;
+return unescape(document.cookie.substring(offset, endstr));
+}
+function GetCookie (name)
+{
+var arg = name + "=";
+var alen = arg.length;
+var clen = document.cookie.length;
+var i = 0;
+while (i < clen)
+{
+var j = i + alen;
+if (document.cookie.substring(i, j) == arg)
+return getCookieVal (j);
+i = document.cookie.indexOf(" ", i) + 1;
+if (i == 0)
+break;
+}
+return null;
+}
+function SetCookie (name, value)
+{
+var argv = SetCookie.arguments;
+var argc = SetCookie.arguments.length;
+var expires = (2 < argc) ? argv[2] : null;
+var path = (3 < argc) ? argv[3] : null;
+var domain = (4 < argc) ? argv[4] : null;
+var secure = (5 < argc) ? argv[5] : false;
+document.cookie = name + "=" + escape (value) + ((expires == null) ? "" : ("; expires=" + expires.toGMTString())) + ((path == null) ? "" : ("; path=" + path)) + ((domain == null) ? "" : ("; domain=" + domain)) + ((secure == true) ? "; secure" : "");
+}
+function DisplayInfo()
+{
+var expdate = new Date();
+var visit;
+expdate.setTime(expdate.getTime()+(24 * 60 * 60 * 1000 * 365));
+if(!(visit = GetCookie("visit")))
+visit = 0;
+visit ++ ;
+SetCookie("visit", visit, expdate, "/", null, false);
+document.write("Usted ha visitado esta pagina "+ visit + " veces"); }
+document.onload=DisplayInfo();
 
-	jQuery(".stat-count").each(function() {
-	  jQuery(this).data('count', parseInt(jQuery(this).md(), 10));
-	  jQuery(this).md('0');
-	  count(jQuery(this));
-	});
-});
+
